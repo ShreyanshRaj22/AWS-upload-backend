ef2fc4a5fc3f89661e783bcb9fc890ee8efdee95
EC2 Default User <ec2-user@ip-172-31-28-125.ec2.internal>
2024-02-08 09:20:40 +0000

    second

--- server.js
+++ server.js
@@ -4,13 +4,20 @@ const path = require('path');
 
 const app = express();
 const port = 8080;
+var cors = require('cors')
+
+app.use(cors())
 
 app.use(fileUpload());
 
 // Serve static files from the 'uploads' directory
 app.use(express.static(path.join(__dirname, 'uploads')));
-
+app.get('/health', (req,res) => {
+	console.log("Up and running!");
+	return res.status(200);
+});
 app.post('/upload', (req, res) => {
+	console.log("Uploading file");
   if (!req.files || Object.keys(req.files).length === 0) {
     return res.status(400).json({ message: 'No files were uploaded.' });
   }
@@ -20,6 +27,7 @@ app.post('/upload', (req, res) => {
 
   uploadedFile.mv(uploadPath, (err) => {
     if (err) {
+	    console.log("err uploading",err);
       return res.status(500).send(err);
     }
 
